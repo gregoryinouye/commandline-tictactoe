b16c729ddd1d91847f72e8551911b8ec8827bf6d
Gregory <gregoryinouye@gmail.com>
2019-07-27 09:27:33 -0500

    draws board after move is made

--- tictactoe.js
+++ tictactoe.js
@@ -15,42 +15,26 @@ const rl = readline.createInterface({
 //   else:
 //     player moves
 
+let playerOne = 'x';
+let playerTwo = 'o';
+let winner = null;
+let isPlayerOneToMove = true;
+
+const board = {
+  1: ' ',
+  2: ' ',
+  3: ' ',
+  4: ' ',
+  5: ' ',
+  6: ' ',
+  7: ' ',
+  8: ' ',
+  9: ' ',
+};
+
 function startGame() {
 
-  let playerOne = 'x';
-  let playerTwo = 'o';
-  let winner = null;
-  let isPlayerOneToMove = true;
-
-  const board = {
-    1: ' ',
-    2: ' ',
-    3: ' ',
-    4: ' ',
-    5: ' ',
-    6: ' ',
-    7: ' ',
-    8: ' ',
-    9: ' ',
-  };
-
-  function renderBoard(board) {
-    console.log(`   ${board[1]} | ${board[2]} | ${board[3]} \n  -----------\n   ${board[4]} | ${board[5]} | ${board[6]} \n  -----------\n   ${board[7]} | ${board[8]} | ${board[9]} `);
-  };
-  
-  function getMove(player) {
-    rl.question('Please select a square (1-9): ', answer => {
-      if (board[answer] === ' ') {
-        board[answer] = player;
-        rl.close();
-      } else {
-        console.log('Invalid selection. Please try again!');
-        getMove(player);
-      }
-    });
-  };
-
-  renderBoard(board);
+  renderBoard();
   getMove(isPlayerOneToMove ? playerOne : playerTwo);
 
   // isPlayerOneToMove = !isPlayerOneToMove;
@@ -58,5 +42,34 @@ function startGame() {
   // add move to board
   // check for win conditions
 };
+
+function renderBoard() {
+  console.log(`   ${board[1]} | ${board[2]} | ${board[3]} \n  -----------\n   ${board[4]} | ${board[5]} | ${board[6]} \n  -----------\n   ${board[7]} | ${board[8]} | ${board[9]} `);
+};
+
+function getMove(player) {
+  rl.question('Please select a square (1-9): ', answer => {
+    if (board[answer] === ' ') {
+      board[answer] = player;
+      isPlayerOneToMove = !isPlayerOneToMove;
+      rl.close();
+      renderBoard();
+    } else {
+      console.log('Invalid selection. Please try again!');
+      renderBoard();
+      getMove(player);
+    }
+  });
+};
+
+function checkGameStatus() {
+  // check for win
+  // if no win but board is full, draw game
+  // otherwise call player turn
+}
+
+function gameOver(winner) {
+  winner === null ? console.log('Game is drawn!') : console.log(`Player ${!isPlayerOneToMove ? playerOne : playerTwo} wins!`);
+}
 
 startGame();
